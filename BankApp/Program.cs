42e29ff2e6458c14e40dd896418bf1f7ce20a1d6
AASaln89 <94790664+AASaln89@users.noreply.github.com>
2023-12-09 22:39:36 +0300

    Fix

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,9 +1,14 @@
-﻿using BankApp.Entities;
+﻿using BankApp;
+using BankApp.Entities;
 using BankApp.Services.Interfaces;
 using BankApp.Utils;
 
 Bank bank = new Bank() { Name = "Bank1" };
 IBankService bankService = DependencyInjecter.GetService<IBankService>();
 bankService.Add(bank);
+Client client = new Client() {Bank = bank, FirstName = "Client1", LastName = "Clnt1", PhoneNumber = "1234321", Number = "1111111", Type = ClientType.Individual};
+
+IClientService clientService = DependencyInjecter.GetService<IClientService>();
+clientService.Add(client);
 
 Console.WriteLine("Hello, World!");
